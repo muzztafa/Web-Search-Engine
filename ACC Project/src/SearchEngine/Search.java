9cb2a93aefdf442971fea2c60c270917e22aff85
Ahmed Mustafa <ahmedmustafa5832@gmail.com>
2022-04-07 02:38:41 -0400

    final search changes

--- ACC Project/src/SearchEngine/Search.java
+++ ACC Project/src/SearchEngine/Search.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 
 public class Search {
 	public static int wordSearch(String data, String word, String fileName) {
-		int counter = 0;
+		int occurences = 0;
 		SearchEngine.BoyerMoore boyerMoore = new SearchEngine.BoyerMoore(word);
 		ArrayList<Integer> offsets = boyerMoore.searchOccurences(data);
-		counter = offsets.size();
-		if (counter != 0) {
-			System.out.println("Found in HTML file --> " + fileName + " --> " + counter + " times"); 
+		occurences = offsets.size();
+		if (occurences != 0) {
+			System.out.println("Found in HTML file --> " + fileName + " --> " + occurences + " times"); 
 			System.out.println("-------------------------------------------------------------------------");
 		}
-		return counter;
+		return occurences;
 	}
 }
